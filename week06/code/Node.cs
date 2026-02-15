15f90a5afa21cea362333a7a815ae2584fc33730
octaviobottari <octaviobottari7@gmail.com>
2026-02-14 21:04:22 -0300

    Activity

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -11,8 +11,7 @@ public class Node
 
     public void Insert(int value)
     {
-        // TODO Start Problem 1
-
+        // TODO Problem 1 
         if (value < Data)
         {
             // Insert to the left
@@ -21,7 +20,7 @@ public class Node
             else
                 Left.Insert(value);
         }
-        else
+        else if (value > Data)  
         {
             // Insert to the right
             if (Right is null)
@@ -29,17 +28,25 @@ public class Node
             else
                 Right.Insert(value);
         }
+
     }
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        // TODO Problem 2 
+        if (value == Data)
+            return true;
+        else if (value < Data)
+            return Left != null && Left.Contains(value);
+        else
+            return Right != null && Right.Contains(value);
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        // TODO Problem 4 
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
